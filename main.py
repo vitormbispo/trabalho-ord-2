42266e11515d72b99f92e83cdef27c03aaa0ebfe
Vitor Bispo <vitorbispo@MacBook-Air-de-Vitor.local>
2025-07-10 23:38:01 -0300

    Remoção finalizada e testes passram

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import io
 import struct
 
 TAM_CAB_BUCKETS:int = 2 # Tamanho do cabeçalho do arquivo de buckets
-TAM_MAX_BUCKET:int = 3
+TAM_MAX_BUCKET:int = 5
 FORMAT_CAMPO:str = "I"
 FORMAT_CAB:str = "H"
 TAM_CAMPO:int = 4
@@ -35,7 +35,7 @@ class Bucket:
         if chaves:
             self.chaves = chaves
         else:
-            self.chaves = [0,0,0]
+            self.chaves = [0]*TAM_MAX_BUCKET
         
     ref:int = 0
     profundidade:int = 0 # 4 bytes
@@ -47,7 +47,7 @@ def inicializar_diretorio():
     dir:Diretorio = Diretorio() # Cria o diretório
     
     arq:io.TextIOWrapper = open(ARQUIVO_BUCKETS,"wb+")
-    arq.write(int.to_bytes(1,TAM_CAB_BUCKETS))
+    arq.write(int.to_bytes(1,TAM_CAB_BUCKETS,signed=True))
         
     novo_bucket:Bucket = Bucket()
     novo_bucket.profundidade = dir.profundidade
@@ -73,11 +73,11 @@ def criar_bucket(dir:Diretorio):
     novo_bucket.profundidade = dir.profundidade
     novo_bucket.ref = quant_buckets
 
-    arq.write(int.to_bytes(novo_bucket.profundidade,TAM_CAMPO))
+    arq.write(int.to_bytes(novo_bucket.profundidade,TAM_CAMPO,signed=True))
     arq.write(int.to_bytes(novo_bucket.quant_chaves,TAM_CAMPO))
 
     for _ in range(TAM_MAX_BUCKET):
-        arq.write(int.to_bytes(0,TAM_CAMPO))
+        arq.write(int.to_bytes(0,TAM_CAMPO,signed=True))
 
     arq.seek(0)
     arq.write(int.to_bytes(quant_buckets+1,TAM_CAB_BUCKETS))
@@ -91,19 +91,21 @@ def carrega_bucket(rrn:int):
     posicao = (rrn*TAM_REG_BUCKET)+TAM_CAB_BUCKETS
     arq.seek(posicao)
 
-    bucket_carregado.profundidade = int.from_bytes(arq.read(TAM_CAMPO))
+    bucket_carregado.profundidade = int.from_bytes(arq.read(TAM_CAMPO),signed=True)
     
     if(bucket_carregado.profundidade == -1): # Bucket excluído
         return None
     
-    bucket_carregado.quant_chaves = int.from_bytes(arq.read(TAM_CAMPO))
+    bucket_carregado.quant_chaves = int.from_bytes(arq.read(TAM_CAMPO),signed=False)
     bucket_carregado.ref = rrn
 
     for i in range(bucket_carregado.quant_chaves):
-        bucket_carregado.chaves[i] = int.from_bytes(arq.read(TAM_CAMPO))
+        bucket_carregado.chaves[i] = int.from_bytes(arq.read(TAM_CAMPO),signed=True)
     return bucket_carregado
 
 def dividir_bucket(bucket:Bucket,diretorio:Diretorio):
+    if bucket.profundidade == diretorio.profundidade:
+        expandir_diretorio(diretorio)
     novo_bucket:Bucket = criar_bucket(diretorio)
     inicio, fim = encontrar_novo_intervalo(bucket,diretorio.profundidade)
 
@@ -111,27 +113,28 @@ def dividir_bucket(bucket:Bucket,diretorio:Diretorio):
         diretorio.buckets[i] = novo_bucket.ref
 
     bucket.profundidade += 1
+    novo_bucket.profundidade = bucket.profundidade
     escrever_bucket(novo_bucket)
-    escrever_bucket(bucket)
-
+    redistribuir_chaves(bucket,diretorio)
+    
 def buscar_chave_bucket(chave:int, bucket:Bucket):
     arq:io.TextIOBase = open(ARQUIVO_BUCKETS,"rb")
     arq.seek(posicao_bucket(bucket)+TAM_CAMPO*2)
 
-    encontrado = int.from_bytes(arq.read(TAM_CAMPO))
+    encontrado = int.from_bytes(arq.read(TAM_CAMPO),signed=True)
     pos_chave = 0
 
     while encontrado != None and pos_chave < bucket.quant_chaves:
         if encontrado == chave:
-            return encontrado, pos_chave # Chave, Foi encontrada?, posição onde encontrou
+            return True, pos_chave # Chave, Foi encontrada?, posição onde encontrou
         else:
             pos_chave += 1
-            encontrado = int.from_bytes(arq.read(TAM_CAMPO))
-    return None, 0
+            encontrado = int.from_bytes(arq.read(TAM_CAMPO),signed=True)
+    return False, 0
 
 def excluir_chave_bucket(chave:int, bucket:Bucket):
     chave_encontrada, pos = buscar_chave_bucket(chave,bucket)
-    if chave_encontrada != None:
+    if chave_encontrada:
         bucket.chaves[pos] = 0
         bucket.quant_chaves -= 1
         deslocar_chaves(bucket.chaves,pos)
@@ -171,9 +174,9 @@ def buscar_chave_diretorio(chave:int,diretorio:Diretorio):
     endereco = gerar_endereco(chave,diretorio.profundidade)
     bucket = carrega_bucket(diretorio.buckets[endereco])
     
-    if bucket == None: return None, 0
+    if bucket == None: return False, None
     
-    encontrada = buscar_chave_bucket(chave,bucket)
+    encontrada,_ = buscar_chave_bucket(chave,bucket)
     return encontrada, bucket
 
 def printar_diretorio(dir:Diretorio):
@@ -199,7 +202,10 @@ def gerar_endereco(chave:int,profundidade:int):
     return val_ret
 
 def inserir_chave(chave:int,dir:Diretorio):
-    #if buscar_chave_diretorio(chave,dir) != None: return False # Chave duplicada
+    chave_existe,_ = buscar_chave_diretorio(chave,dir)
+    if chave_existe: 
+        print("Chave duplicada")
+        return False # Chave duplicada
 
     end = gerar_endereco(chave,dir.profundidade)
     bucket:Bucket = carrega_bucket(dir.buckets[end])
@@ -209,19 +215,15 @@ def inserir_chave(chave:int,dir:Diretorio):
         bucket.quant_chaves += 1
         escrever_bucket(bucket)
     else:
-        if bucket.profundidade >= dir.profundidade:
-            expandir_diretorio(dir)
-            inserir_chave(chave,dir)
-        else:
-            dividir_bucket(bucket,dir)
-            redistribuir_chaves(bucket,dir)
-            inserir_chave(chave,dir)
+        dividir_bucket(bucket,dir)
+        inserir_chave(chave,dir)
+    printar_diretorio(dir)
     return True
 
 def redistribuir_chaves(bucket:Bucket,diretorio:Diretorio):
-    chaves = bucket.chaves.copy()
-    for i in range(len(chaves)):
-        excluir_chave_bucket(chaves[i],bucket) # Esvazia o bucket
+    chaves = bucket.chaves[0:bucket.quant_chaves]
+    for chave in chaves:
+        excluir_chave_bucket(chave,bucket) # Esvazia o bucket
     for chave in chaves:
         inserir_chave(chave,diretorio) # Redistribui
 
@@ -246,20 +248,35 @@ def excluir_chave(chave:int,dir:Diretorio):
     endereco = gerar_endereco(chave,dir.profundidade)
     bucket:Bucket = carrega_bucket(dir.buckets[endereco])
 
+    if bucket == None: return False
+
     if excluir_chave_bucket(chave,bucket):
-        tem_amigo, amigo = encontrar_bucket_amigo(bucket,dir)
-        if tem_amigo:
-            bucket_amigo = carrega_bucket(amigo)
-            if(bucket_amigo.quant_chaves + bucket.quant_chaves <= TAM_MAX_BUCKET):
-                combinado = concatena_buckets(bucket,bucket_amigo)
-                escrever_bucket(combinado)
-                dir.buckets[amigo] = endereco
-                excluir_bucket(bucket_amigo)
+        tentar_combinar_buckets(bucket,endereco,dir)
+        return True
+    return False
+
+
+def tentar_combinar_buckets(bucket:Bucket,endereco:int,dir:Diretorio):
+    tem_amigo, amigo = encontrar_bucket_amigo(bucket,dir)
+    if tem_amigo:
+        bucket_amigo = carrega_bucket(dir.buckets[amigo])
+        if(bucket_amigo.quant_chaves + bucket.quant_chaves <= TAM_MAX_BUCKET):
+            combinado = concatena_buckets(bucket,bucket_amigo)
+            excluir_bucket(bucket_amigo)
+            escrever_bucket(combinado)
+            dir.buckets[amigo] = dir.buckets[endereco]
+            
+
+            if tentar_reduzir_diretorio(dir):
+                for i in range(0,len(dir.buckets),2):
+                    tentar_combinar_buckets(carrega_bucket(dir.buckets[i]),i,dir)
+            return True
+    return False
 
 def excluir_bucket(bucket:Bucket):
     arq:io.TextIOWrapper = open(ARQUIVO_BUCKETS,"rb+")
     arq.seek(posicao_bucket(bucket))
-    arq.write(int.to_bytes(-1,TAM_CAMPO))
+    arq.write(int.to_bytes(-1,TAM_CAMPO,signed=True))
 
 def concatena_buckets(bucket1:Bucket,bucket2:Bucket):
     novo_bucket = Bucket()
@@ -267,6 +284,8 @@ def concatena_buckets(bucket1:Bucket,bucket2:Bucket):
     novo_bucket.quant_chaves = bucket1.quant_chaves + bucket2.quant_chaves
     novo_bucket.ref = bucket1.ref
     novo_bucket.chaves = bucket1.chaves[0:bucket1.quant_chaves] + bucket2.chaves[0:bucket2.quant_chaves]
+    novo_bucket.chaves += [0]*(TAM_MAX_BUCKET-novo_bucket.quant_chaves)
+    
     return novo_bucket
 
 def encontrar_bucket_amigo(bucket:Bucket,dir:Diretorio):
@@ -279,6 +298,8 @@ def encontrar_bucket_amigo(bucket:Bucket,dir:Diretorio):
 
 def tentar_reduzir_diretorio(dir:Diretorio):
     novas_referencias = []
+    if dir.profundidade == 0: return False
+
     for i in range(0,len(dir.buckets),2):
         if dir.buckets[i] == dir.buckets[i+1]:
             novas_referencias.append(dir.buckets[i])
@@ -286,27 +307,49 @@ def tentar_reduzir_diretorio(dir:Diretorio):
             return False
     # Reduzindo
     dir.buckets = novas_referencias
-    return True
+    dir.profundidade -= 1
 
-#
-#    000 -> 
-#    001 ->
-#    010 ->
-#    011 -> 
+    return not tentar_reduzir_diretorio(dir)
+
+# Funções de execução =========================================================================================================
+
+def executar_operacoes(nome_arquivo:str,dir:Diretorio):
+    arq:io.TextIOWrapper = open(nome_arquivo,"r")
+    log:io.TextIOWrapper = open(f"log-{nome_arquivo}","w")
+    linha = arq.readline()
+    
+    while linha:
+        campos = linha.split(" ")
+        operacao = campos[0]
+        entrada = int(campos[1])
+        match operacao:
+            case "i":
+                log.write(f"Inserção da chave {entrada}: ")
+                if inserir_chave(entrada,dir):
+                    log.write("Sucesso.\n")
+                else:
+                    log.write("Falha – Chave duplicada.\n")
+            case "b":
+                encontrou, bucket = buscar_chave_diretorio(entrada,dir)
+                log.write(f"Busca pela chave {entrada}: ")
+                if encontrou:
+                    log.write(f"Chave encontrada no bucket {bucket.ref}.\n")
+                else:
+                    log.write("Chave não encontrada.\n")
+            case "r":
+                log.write(f"Remoção da chave {entrada}: ")
+                if excluir_chave(entrada,dir):
+                    log.write(f"Sucesso.\n")
+                else:
+                    log.write(f"Falha - Chave não encontrada.\n")
+        linha = arq.readline()
+    arq.close()
+    log.close()
+            
+
 def main():
     dir = inicializar_diretorio()
-
-    inserir_chave(1,dir)
-    inserir_chave(2,dir)
-    inserir_chave(3,dir)
-    inserir_chave(4,dir)
-    
-    printar_diretorio(dir)
-
-    expandir_diretorio(dir)
-
-    printar_diretorio(dir)
-    tentar_reduzir_diretorio(dir)
+    executar_operacoes("op100.txt",dir)
     printar_diretorio(dir)
 
 if __name__ == "__main__":
